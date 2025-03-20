f55dd8384e446bd8601891fd55e3830ff57abee5
bhaukal01 <narayanadi2017@gmail.com>
2025-03-20 22:04:40 +0530

    new

--- routes/success.js
+++ routes/success.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Purchase = require("../models/Purchase"); // Assuming you have a Purchase model
+const Purchase = require("../models/purchaseModel");; // Assuming you have a Purchase model
 
 router.post("/pay-success", async (req, res) => {
     try {
